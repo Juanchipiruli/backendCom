ed42aca921018cc64ed1d028a5216267a2cbdd66
Juanchipiruli <jbruera@facultad.sanfrancisco.utn.edu.ar>
2025-08-05 20:28:21 -0300

    que capos que somos dios mio

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
-const {User, Aula} = require('../models');
+const {User, Aula, Horario, Materia, Catedra} = require('../models');
 const { getIO } = require('../socket');
+const {Op} = require('sequelize')
 
 const obtenerUser = async (req, res) => {
     try{
@@ -43,8 +44,8 @@ const validateProfe = async (req, res) => {
         const aulaExiste = await Aula.findOne({where: {sensorId: sensorId}});
         const profeExiste = await User.findOne({where: {huellaId: huellaId}});
 
-        if(!aulaExiste) return res.status(400).json({messagge: "No existe el aula"});
-        if(!profeExiste) return res.status(400).json({messagge: "No existe profe"});
+        if(!aulaExiste) return res.status(400).json({messagge: "No existe el aula", valido: false});
+        if(!profeExiste) return res.status(400).json({messagge: "No existe profe", valido: false});
 
         const now = new Date();
         const diaSemana = now.getDay();
@@ -60,9 +61,33 @@ const validateProfe = async (req, res) => {
         });
         
         if(!horarioCoincidente) {
-            return res.status(404).json({messagge: "No hay horario programado para esta aula en este momento"});
+            return res.status(404).json({messagge: "No hay horario programado para esta aula en este momento", valido: false});
         }
 
+        const materiaExiste = await Materia.findByPk(horarioCoincidente.materiaId);
+        if(!materiaExiste) return res.status(400).json({messagge:"El horario no coincide con la materia", valido: false});
+
+        const catedraExiste = await Catedra.findOne({where: {materiaId: materiaExiste.id, userId: profeExiste.id}});
+
+        if(catedraExiste){
+            io.emit('aula_abrir', {
+                aula: aulaExiste.id,
+                nombreAula: aulaExiste.nombre,
+                nombre: profeExiste.nombre,
+                valido: true
+            });
+            return res.status(200).json({messagge: "Profe valido", valido: true});
+        }else if(!catedraExiste){
+            io.emit('aula_no_abrir', {
+                aula: aulaExiste.id,
+                nombreAula: aulaExiste.nombre,
+                nombre: profeExiste.nombre,
+                valido: false
+            });
+            return res.status(400).json({messagge: "No existe una catedra para esta materia", valido: false});
+        }
+        return res.status(400).json({valido: false});
+        
     }catch(error){
         return res.status(500).json({messagge: error.messagge});
     }
@@ -135,5 +160,6 @@ module.exports = {
     getAllUsers,
     obtenerUserHuella,
     deleteUser,
-    updateUser
+    updateUser,
+    validateProfe
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,14 +6,16 @@ const {
     getAllUsers,
     obtenerUserHuella,
     deleteUser,
-    updateUser
+    updateUser,
+    validateProfe
 } = require('../controllers/user.controller')
 
 router.get('/id=:userId', obtenerUser);
 router.post('/huella=:huellaId', createUser);
-router.get('/', getAllUsers)
-router.get('/huella=:huellaId',obtenerUserHuella)
-router.delete('/id=:userId', deleteUser)
-router.put('/id=:userId', updateUser)
+router.get('/', getAllUsers);
+router.get('/huella=:huellaId',obtenerUserHuella);
+router.delete('/id=:userId', deleteUser);
+router.put('/id=:userId', updateUser);
+router.post('/validate/sensor=:sensorId', validateProfe)
 
 module.exports= router;
